5db422b48a6ea1b84516bf256ccd2cf438d8bbb5
tln208 <tln208@alumni.ku.dk>
2024-06-21 12:00:26 +0200

    Notebook done

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,6 +1,6 @@
 from types import SimpleNamespace
 import numpy as np
-from scipy import minimize
+import matplotlib.pyplot as plt
 
 class ExchangeEconomyClass:
 
@@ -96,12 +96,38 @@ class ExchangeEconomyClass:
 
         return [D1A + D1B -1, D2A + D2B - 1]
 
-    def find_equilibrium_numer(self):
-        ini_guess = 1.0
+    def edgeworth_box(self, title, pad):
+        # a. total endowment
+        w1bar = 1.0
+        w2bar = 1.0
 
-        eq_price = minimize(lambda p1: p1, ini_guess, constraints={'fun': self.random_demand_con, 'type': 'eq'})
-        return eq_price
+        # b. figure set up
+        fig = plt.figure(frameon=False,figsize=(6,6), dpi=100)
+        ax_A = fig.add_subplot(1, 1, 1)
+        ax_A.set_title(title, pad = pad)
 
+        ax_A.set_xlabel("$x_1^A$")
+        ax_A.set_ylabel("$x_2^A$")
+
+        temp = ax_A.twinx()
+        temp.set_ylabel("$x_2^B$")
+        ax_B = temp.twiny()
+        ax_B.set_xlabel("$x_1^B$")
+        ax_B.invert_xaxis()
+        ax_B.invert_yaxis()
+
+        # limits
+        ax_A.plot([0,w1bar],[0,0],lw=2,color='black')
+        ax_A.plot([0,w1bar],[w2bar,w2bar],lw=2,color='black')
+        ax_A.plot([0,0],[0,w2bar],lw=2,color='black')
+        ax_A.plot([w1bar,w1bar],[0,w2bar],lw=2,color='black')
+
+        ax_A.set_xlim([-0.1, w1bar + 0.1])
+        ax_A.set_ylim([-0.1, w2bar + 0.1])    
+        ax_B.set_xlim([w1bar + 0.1, -0.1])
+        ax_B.set_ylim([w2bar + 0.1, -0.1])
+
+        return ax_A
 
 
 
